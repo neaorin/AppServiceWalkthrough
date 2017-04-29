cc9958d1e65271361cb4d7ed72a7190d030eb935
Sorin Peste <neaorin@gmail.com>
2017-04-29 22:18:53 +0300

    inital app code

---
+++ README.md
@@ -0,0 +1,3 @@
+# App Service Walkthrough
+
+This is a walktrough of the Azure App Service features.
---
+++ src/initial/MercuryHealth.Web/Utilities/TelemetryClientHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MercuryHealth.Web.Utilities
+{
+    public static class TelemetryClientHelper
+    {
+        public static void TrackEvent(string description)
+        {
+            // Uncomment these lines to track custom events.
+
+            // var telemetry = new Microsoft.ApplicationInsights.TelemetryClient();
+            // telemetry.TrackEvent(description);
+        }
+    }
+}
